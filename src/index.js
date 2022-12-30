f6d429fcb336c38972aeeb0ebac4ed7323a702d0
Arian Hosseini <arianhosseini@outlook.com>
2022-12-30 17:59:44 +0330

    feat: added jest as a new Hardhat task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 const { task, subtask } = require("hardhat/config");
 const { TASK_COMPILE } = require("hardhat/builtin-tasks/task-names");
 const { runCLI } = require("jest");
+
+task("jest", "Runs Jest tests")
+	.addFlag("noCompile", "Do NOT compile before running this task")
+	.addFlag("watch", "Watch files for changes and rerun tests related to changed files.")
+	.addFlag("watchAll", "Watch files for changes and rerun all tests")
+	.setAction(async (taskArgs, { run }) => {
+		const { watch: watchFlag, watchAll: watchAllFlag, noCompile: noCompileFlag } = taskArgs;
+
+		// If --no-compile flag is added, then don't compile before running this task
+		if (!noCompileFlag) {
+			await run(TASK_COMPILE, { quiet: true });
+		}
+
+		// Call suntask jest:run
+		await run("jest:run", { watchFlag, watchAllFlag });
+	});
