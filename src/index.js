dca70272f9f27654a08b4d6e424d41e4dd688fd7
Arian Hosseini <arianhosseini@outlook.com>
2023-01-03 15:42:31 +0330

    feat: added showConfig flag

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const { task, subtask } = require("hardhat/config");
 const { TASK_COMPILE } = require("hardhat/builtin-tasks/task-names");
 const { runCLI } = require("jest");
 
+let projectRootPath;
+
 task("jest", "Runs Jest tests")
 	.addFlag("noCompile", "Do NOT compile before running this task.")
 	.addFlag(
@@ -10,26 +12,31 @@ task("jest", "Runs Jest tests")
 	)
 	.addFlag("watchAll", "Watch files for changes and rerun all tests.")
 	.addFlag("bail", "Stop running tests after the first test failure.")
+	.addFlag("showConfig", "Print your jest config and then exits.")
 	.setAction(async (taskArgs, { run }) => {
 		const {
 			watch: watchFlag,
 			watchAll: watchAllFlag,
 			noCompile: noCompileFlag,
 			bail: bailFlag,
+			showConfig,
 		} = await taskArgs;
 
+		projectRootPath = [config.paths.root];
+
 		// If --no-compile flag is added, then don't compile before running this task
-		if (!noCompileFlag) {
+		if (!noCompileFlag && !showConfig) {
 			await run(TASK_COMPILE, { quiet: true });
 		}
+
+		// If --show-config is added, it prints the jest config and then exits
+		showConfig ? await run("jest:showConfig") : null;
 
 		// Call suntask jest:run
 		await run("jest:run", { watchFlag, watchAllFlag, bailFlag });
 	});
 
 subtask("jest:run").setAction(async ({ watchFlag, watchAllFlag, bailFlag }) => {
-	const projectRootPath = [config.paths.root];
-
 	const jestOptions = {
 		setupFiles: ["hardhat/register"],
 		watch: watchFlag,
@@ -38,6 +45,15 @@ subtask("jest:run").setAction(async ({ watchFlag, watchAllFlag, bailFlag }) => {
 	};
 
 	await runCLI(jestOptions, projectRootPath)
+		.then((result) => result)
+		.catch((error) => console.error(error));
+});
+
+subtask("jest:showConfig").setAction(async () => {
+	await runCLI(
+		{ setupFiles: ["hardhat/register"], showConfig: true },
+		projectRootPath
+	)
 		.then((result) => result)
 		.catch((error) => console.error(error));
 });
